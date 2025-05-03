e53fa8325ba2164161608d0c53db868c62171f20
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2025-05-03 10:11:39 -0500

    deploy

--- Doc2Markdown/run.py
+++ Doc2Markdown/run.py
@@ -3,10 +3,8 @@ from app.app import app
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.app:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
-        #ssl_keyfile="./key.pem",
-        #ssl_certfile="./cert.pem"
-    )
+    "app.app:app",
+    host="0.0.0.0",  # Cambiar a 0.0.0.0 en producción
+    port=8000,
+    reload=False  # Deshabilitar recarga en producción
+)
---
+++ Doc2Markdown/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Activar el entorno virtual
+source /home/site/wwwroot/venv/bin/activate
+
+# Ejecutar la aplicación con uvicorn
+/home/site/wwwroot/venv/bin/python3 /home/site/wwwroot/run.py
