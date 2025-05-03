20da38db6de1de3178fb4b235e314121015206e1
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2025-05-03 13:08:38 -0500

    Subida de documentos FD01,02,03 Y 04

--- Doc2Markdown/run.py
+++ Doc2Markdown/run.py
@@ -8,3 +8,17 @@ if __name__ == "__main__":
     port=8000,
     reload=False  # Deshabilitar recarga en producción
 )
+    
+# CONFIGURACION LOCAL
+#   import uvicorn
+#from app.app import app
+
+#if _name_ == "_main_":
+#    uvicorn.run(
+#        "app.app:app",
+#        host="127.0.0.1",
+#        port=8000,
+#        reload=True
+#        #ssl_keyfile="./key.pem",
+#        #ssl_certfile="./cert.pem"
+    )
