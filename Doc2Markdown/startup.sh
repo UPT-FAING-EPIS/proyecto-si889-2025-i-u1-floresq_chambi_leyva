b35ee03e290cf988b56c0b0409a19c2ed7584d44
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2025-05-03 11:36:17 -0500

    deploy

--- Doc2Markdown/startup.sh
+++ Doc2Markdown/startup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Navegar al directorio donde está la aplicación
 cd /home/site/wwwroot
 
-# Instalar las dependencias si es necesario
-pip install --no-cache-dir -r requirements.txt
-
-# Ejecutar la aplicación con uvicorn a través del script run.py
-python run.py
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.app:app --bind 0.0.0.0:8000 --timeout 600
